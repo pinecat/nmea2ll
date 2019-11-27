700b2998f51d39dee9e19bb83fc6f1be1e4c9a43
rory <rory.dudley@gmail.com>
2019-11-27 12:02:05 -0500

    fixing lat and long parsing

--- nmea2ll.py
+++ nmea2ll.py
@@ -11,12 +11,13 @@
 
 # define dictionary and helper function for valid sentence types
 #   (i.e. sentence types with lat and long data)
-def parse_nmea_sentences(nmea_sentence_type, sentence):
+def parse_nmea_sentences(sentence):
     sentences = {
         "GLL": parse_gll, # "GLL: Geographic Position - Latitude/Longitude"
         "GGA": parse_gga, # "GGA: Global Positioning System Fix Data"
         "RMC": parse_rmc  # "RMC: Recommended Minimum Navigation Information"
     }
+    nmea_sentence_type = sentence[3:6]
     func = sentences.get(nmea_sentence_type, parse_others)
     return func(sentence)
 
@@ -24,39 +25,72 @@ def parse_nmea_sentences(nmea_sentence_type, sentence):
 #   parse lat and long from GLL sentence
 def parse_gll(sentence):
     data = sentence.split(',')          # split data into a list
-    lat = data[1] + data[2]             # get lat data at correct position in list
-    long = data[3] + data[4]            # get long data at correct position in list
-    lat = lat.replace('.', '')          # remove decimal point from incorrect location in lat
-    long = long.replace('.','')         # remove decimal point from incorrect location in long
-    lat = lat[:2] + '.' + lat[2:]       # add decimal point back to correct location in lat
-    long = long[:3] + '.' + long[3:]    # add decimal point back to correct location in long
-    location = "{ \"GLL\": { \"lat\": \"" + lat + "\", \"long\": \"" + long + "\" } }"  # lat and long put into structured JSON
+    lat = data[1]                       # get lat data at correct position in list
+    long = data[3]                      # get long data at correct position in list
+    lat_dir = data[2]                   # get N, S
+    long_dir = data[4]                  # get E, W
+    lat_deg = float(lat[:2])            # parse degrees from latitude
+    lat_min = float(lat[2:])            # parse minutes from latitude
+    long_deg = float(long[:3])          # parse degrees from longitude
+    long_min = float(long[3:])          # parse minutes from longitude
+    lat = lat_deg + (lat_min/60.0)      # convert to decimal representation of lat
+    long = long_deg + (long_min/60.0)   # convert to decimal representation of long
+
+    # make sure decimal representation is correct depending on N, S, E, W
+    if lat_dir == 'S':
+        lat = lat * -1
+    if long_dir == 'W':
+        long = long * -1
+
+    location = "{ \"GLL\": { \"lat\": \"" + str(lat) + "\", \"long\": \"" + str(long) + "\" } }"  # lat and long put into structured JSON
     return location                     # return the location
 
 # parse_gga
 #   parse lat and long from GGA sentence
 def parse_gga(sentence):
     data = sentence.split(',')          # split data into a list
-    lat = data[2] + data[3]             # get lat data at correct position in list
-    long = data[4] + data[5]            # get long data at correct position in list
-    lat = lat.replace('.', '')          # remove decimal point from incorrect location in lat
-    long = long.replace('.','')         # remove decimal point from incorrect location in long
-    lat = lat[:2] + '.' + lat[2:]       # add decimal point back to correct location in lat
-    long = long[:3] + '.' + long[3:]    # add decimal point back to correct location in long
-    location = "{ \"GGA\": { \"lat\": \"" + lat + "\", \"long\": \"" + long + "\" } }"  # lat and long put into structured JSON
+    lat = data[2]                       # get lat data at correct position in list
+    long = data[4]                      # get long data at correct position in list
+    lat_dir = data[3]                   # get lat data at correct position in list
+    long_dir = data[5]                  # get long data at correct position in list
+    lat_deg = float(lat[:2])            # parse degrees from latitude
+    lat_min = float(lat[2:])            # parse minutes from latitude
+    long_deg = float(long[:3])          # parse degrees from longitude
+    long_min = float(long[3:])          # parse minutes from longitude
+    lat = lat_deg + (lat_min/60.0)      # convert to decimal representation of lat
+    long = long_deg + (long_min/60.0)   # convert to decimal representation of long
+
+    # make sure decimal representation is correct depending on N, S, E, W
+    if lat_dir == 'S':
+        lat = lat * -1
+    if long_dir == 'W':
+        long = long * -1
+
+    location = "{ \"GGA\": { \"lat\": \"" + str(lat) + "\", \"long\": \"" + str(long) + "\" } }"  # lat and long put into structured JSON
     return location                     # return the location
 
 # parse_rmc
 #   parse lat and long from RMC sentence
 def parse_rmc(sentence):
     data = sentence.split(',')          # split data into a list
-    lat = data[3] + data[4]             # get lat data at correct position in list
-    long = data[5] + data[6]            # get long data at correct position in list
-    lat = lat.replace('.', '')          # remove decimal point from incorrect location in lat
-    long = long.replace('.','')         # remove decimal point from incorrect location in long
-    lat = lat[:2] + '.' + lat[2:]       # add decimal point back to correct location in lat
-    long = long[:3] + '.' + long[3:]    # add decimal point back to correct location in long
-    location = "{ \"RMC\": { \"lat\": \"" + lat + "\", \"long\": \"" + long + "\" } }"  # lat and long put into structured JSON
+    lat = data[3]                       # get lat data at correct position in list
+    long = data[5]                      # get long data at correct position in list
+    lat_dir = data[4]                   # get lat data at correct position in list
+    long_dir = data[6]                  # get long data at correct position in list
+    lat_deg = float(lat[:2])            # parse degrees from latitude
+    lat_min = float(lat[2:])            # parse minutes from latitude
+    long_deg = float(long[:3])          # parse degrees from longitude
+    long_min = float(long[3:])          # parse minutes from longitude
+    lat = lat_deg + (lat_min/60.0)      # convert to decimal representation of lat
+    long = long_deg + (long_min/60.0)   # convert to decimal representation of long
+
+    # make sure decimal representation is correct depending on N, S, E, W
+    if lat_dir == 'S':
+        lat = lat * -1
+    if long_dir == 'W':
+        long = long * -1
+
+    location = "{ \"RMC\": { \"lat\": \"" + str(lat) + "\", \"long\": \"" + str(long) + "\" } }"  # lat and long put into structured JSON
     return location                     # return the location
 
 # parse_others
@@ -70,11 +104,10 @@ nmea_data = open("20191121-18-44-20.nmea", "r")
 # loop through each line in the nmea data file
 #   (each line will be 1 nmea sentence)
 for sentence in nmea_data:
-    if sentence[0] == "$":                                          # only check nmea statement if sentence begins with a '$'
-        nmea_sentence_type = sentence[3:6]                          # use substring to get the sentence type
-        loc = parse_nmea_sentences(nmea_sentence_type, sentence)    # will return location if sentence type is correct (i.e. GLL, GGA, or RMC)
-        if not loc == "":                                           # if the location is not empty...
-            print(loc)                                              # print the location
+    if sentence[0] == "$":                      # only check nmea statement if sentence begins with a '$'
+        loc = parse_nmea_sentences(sentence)    # will return location if sentence type is correct (i.e. GLL, GGA, or RMC)
+        if not loc == "":                       # if the location is not empty...
+            print(loc)                          # print the location
 
 # close the nmea data file now that we are finished with it
 nmea_data.close()
