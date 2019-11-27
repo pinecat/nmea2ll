f043f9530ad89d391e1e3891bc30e5ecc7439479
pinecat <rory.dudley@gmail.com>
2019-11-26 23:16:12 -0500

    saving code, coming back to fix json

--- bot.py
+++ bot.py
@@ -1,4 +1,13 @@
 import RPi.GPIO as GPIO
 import time
-from nmea2ll import parse_nmea_sentence(nmea_sentence_type, sentence)
+import json
+from nmea2ll import parse_nmea_sentences
 
+# open pathdata file for route
+path_data = open('pathdata.json', 'r')
+
+for data in path_data:
+    loc = json.loads(data)
+    print(loc)
+
+path_data.close()
