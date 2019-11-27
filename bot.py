600d37566cee90264289a631fa35c27cba474c90
pinecat <rory.dudley@gmail.com>
2019-11-26 23:34:39 -0500

    fixing json in nmea2ll.py

--- bot.py
+++ bot.py
@@ -3,21 +3,36 @@ import time
 import json
 from nmea2ll import parse_nmea_sentences
 
+HOST = '192.168.0.11'
+PORT_LL = 12345
+PORT_HEADING = 54321
+
 # open pathdata file for route
 path_data = open('pathdata.json', 'r')
 
+# open data sockets
+ll_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ll_sock.connect((HOST, PORT_LL))
+heading_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+heading_sock.connect((HOST, PORT_HEADING))
+
 for data in path_data:
     ll = json.loads(data)
-    lat
-    long
+    lat = 0
+    long = 0
 
     if ll.get('GLL'):
-        print ll['GLL']
+        lat = ll['GLL']['lat']
+        long = ll['GLL']['long']
 
     if ll.get('GGA'):
-        print ll['GGA']
+        lat = ll['GGA']['lat']
+        long = ll['GGA']['long']
 
     if ll.get('RMC'):
-        print ll['RMC']
+        lat = ll['RMC']['lat']
+        long = ll['RMC']['long']
+
+    
 
 path_data.close()
